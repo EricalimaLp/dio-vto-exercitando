40be0f06aaeab6b6e404de2257805ea0f0eb4fba
Erica <ericasilva84@yahoo.com.br>
2023-01-05 00:15:56 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+/**
+ * Close de exemplo para o exercicia da aula 1 de variáveis, tipos de dado e Operadores aritmeticos.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    int i;
+    //int i;
+    int I;
+    //int 1a;
+    int _1a;
+    int $aq;
+
+    i = 5;
+    I = 10;
+    _1a = 20;
+    $aq = 7;
+
+    final int j = 10;
+    //j = 15;
+    int asrn24678md;
+    //int asrn246 78md;
+    int asrn24678_md = 10;
+    //int asrn2$4678_md = 10;
+
+    asrn24678md = 100;
+    asrn24678_md = 10;
+
+    int quantidadeProduto = 50;
+    //int QuantidadeProduto;
+    final int NUMERO_TENTATIVAS = 5;
+    //final int numeroTentativas = 5;
+    int Quantidade_OPCOES = 25;
+    //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(Quantidade_OPCOES);
+
+    }
+}
